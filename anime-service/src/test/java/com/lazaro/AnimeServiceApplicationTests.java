cc6d04f09114395d0ea7ab338f410a56a687a59c
Lázaro <lazarovasconceloscoutinho@gmail.com>
2024-10-04 18:44:25 -0300

    commit inicial

---
+++ anime-service/src/test/java/com/lazaro/AnimeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
